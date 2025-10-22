1c40ebf12e280b46f24d32a184afc4ebde6b714d
fedegiovannetti <giovannettipsi@gmail.com>
2025-10-22 00:22:16 -0300

    Github actions fully working (fingers crossed)

--- references/1-fetch_orcid_data.R
+++ references/1-fetch_orcid_data.R
@@ -1,28 +1,29 @@
-# --- SCRIPT 1: OBTENER DATOS DE ORCID ---
 
 library(rorcid)
 library(dplyr)
 
-print("--- PASO 1: Iniciando fetch de ORCID ---")
+#### This script contains the code for retrieving references
+#### from articles, conferences and chapters from Orcid
+
+## My Orcid id
 
 orcid_id = "0000-0003-2238-3674"
 
-# 1. Obtener 'works'
-# La autenticación (ORCID_TOKEN) es usada aquí
-print("Obteniendo works()...")
+# Retrieve all works
+
 orcid_works_all <- works(orcid_id) %>% 
+  
+  # I simplify a little this data as I really don't need so much columns
   as.data.frame() %>% 
   select('put-code', type, title.title.value, url.value, 'publication-date.year.value', 'journal-title.value')
-print("... works() obtenidos.")
 
-# 2. Obtener 'citations'
-# La autenticación (ORCID_TOKEN) es usada aquí
-print("Obteniendo orcid_citations()...")
+
+# Retrieve all citations (this is a different way of accessing the data
+# and gives more-formatted citations)
+
 all_citations <- rorcid::orcid_citations(orcid_id)
-print("... citations obtenidas.")
 
-# 3. Guardar en archivos temporales
+
+# save all in temporary files for github actions
 saveRDS(orcid_works_all, file = "orcid_works_all.rds")
 saveRDS(all_citations, file = "all_citations.rds")
-
-print("--- PASO 1: Datos de ORCID guardados en .rds ---")
